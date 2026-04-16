ef1685ad8644ef1a350e8684951c6bea56cff96a
marut <marutnamdeo03@gmail.com>
2026-04-16 16:54:39 +0530

    Initialized Spring Boot project with dependencies

---
+++ src/test/java/com/todo/smarttodo/SmarttodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.smarttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarttodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
